9117b4afa696c83983a8cd66333e13211163b704
pateluday07 <pateluday07@gmail.com>
2025-09-14 14:40:52 +0530

    develop initial commit

---
+++ src/main/java/com/byteandbeyondwithuday/springbootrestapi/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.byteandbeyondwithuday.springbootrestapi.repository;
+
+import com.byteandbeyondwithuday.springbootrestapi.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
--- src/main/java/com/byteandbeyondwithuday/springbootrestapi/service/EmployeeServiceImpl.java
+++ src/main/java/com/byteandbeyondwithuday/springbootrestapi/service/EmployeeServiceImpl.java
@@ -1,64 +1,46 @@
 package com.byteandbeyondwithuday.springbootrestapi.service;
 
 import com.byteandbeyondwithuday.springbootrestapi.entity.Employee;
+import com.byteandbeyondwithuday.springbootrestapi.repository.EmployeeRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private static final Map<Long, Employee> employeeDB = new LinkedHashMap<>();
-    private static Long idCounter = 0L;
+    private final EmployeeRepository employeeRepository;
 
-    public EmployeeServiceImpl() {
-        Employee employee = new Employee(getNextId(), "John", "Doe", 50000.0);
-        employeeDB.put(employee.getId(), employee);
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     public void save(Employee employee) {
-        employee.setId(getNextId());
-        employeeDB.put(employee.getId(), employee);
+        employeeRepository.save(employee);
     }
 
     @Override
     public Employee update(Employee employee) {
-        checkEmployeeExists(employee.getId());
-        employeeDB.put(employee.getId(), employee);
-        return employee;
+        return employeeRepository.save(employee);
     }
-
 
     @Override
     public Employee findById(Long id) {
-        checkEmployeeExists(id);
-        return employeeDB.get(id);
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @Override
     public void deleteById(Long id) {
-        checkEmployeeExists(id);
-        employeeDB.remove(id);
+        employeeRepository.deleteById(id);
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDB.values()
-                .stream()
-                .toList();
+        return employeeRepository.findAll();
     }
 
-    private void checkEmployeeExists(Long id) {
-        if (!employeeDB.containsKey(id))
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-    }
-
-    private static Long getNextId() {
-        return ++idCounter;
-    }
 }
